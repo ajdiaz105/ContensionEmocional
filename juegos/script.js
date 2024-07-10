396646a91b5e2e926113898ceb11bd1f1e817ea2
Ajdiazs <ajdiazs@puce.edu.ec>
2024-07-09 20:51:34 -0500

    final

--- juegos/script.js
+++ juegos/script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const container = document.getElementById('circle-container');
-    const maxCircles = 30;
+    const maxCircles = 20;
     let circleCount = 0;
 
     container.addEventListener('click', function(event) {
